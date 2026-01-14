b3fcd45cdaf6c97f93b795c828e64ff4bdf4d03f
lexi-rademann <voight.lexi@gmail.com>
2026-01-14 09:18:59 -0600

    Update BackgroundDecor.tsx

--- components/BackgroundDecor.tsx
+++ components/BackgroundDecor.tsx
@@ -5,25 +5,20 @@ export function BackgroundDecor() {
     // 15 images total - each appears 3 times, well spaced
     // Row 1 - top
     { src: "/images/martini.png", top: "10%", left: "3%", size: 130, rotate: -12, opacity: 0.18 },
-    { src: "/images/trees.png", top: "4%", left: "50%", size: 140, rotate: 8, opacity: 0.45 },
     { src: "/images/wineglasses.png", top: "10%", left: "88%", size: 130, rotate: 10, opacity: 0.55 },
     // Row 2
+    { src: "/images/trees.png", top: "25%", left: "15%", size: 130, rotate: -3, opacity: 0.50 },
     { src: "/images/ring.png", top: "20%", left: "15%", size: 120, rotate: -5, opacity: 0.25 },
-    { src: "/images/bonfire.png", top: "22%", left: "70%", size: 130, rotate: 12, opacity: 0.18 },
     // Row 3
     { src: "/images/wineglasses.png", top: "38%", left: "2%", size: 130, rotate: 10, opacity: 0.55 },
-    { src: "/images/bonfire.png", top: "30%", left: "30%", size: 130, rotate: 15, opacity: 0.18 },
     { src: "/images/ring.png", top: "38%", left: "85%", size: 120, rotate: -8, opacity: 0.25 },
     // Row 4
     { src: "/images/trees.png", top: "50%", left: "15%", size: 130, rotate: -3, opacity: 0.50 },
-    { src: "/images/martini.png", top: "48%", left: "50%", size: 130, rotate: 8, opacity: 0.18 },
     // Row 5
     { src: "/images/bonfire.png", top: "76%", left: "3%", size: 130, rotate: -10, opacity: 0.18 },
-    { src: "/images/wineglasses.png", top: "75%", left: "42%", size: 130, rotate: 5, opacity: 0.55 },
     { src: "/images/trees.png", top: "53%", left: "78%", size: 130, rotate: 12, opacity: 0.50 },
     // Row 6 - bottom
     { src: "/images/martini.png", top: "90%", left: "25%", size: 130, rotate: 8, opacity: 0.18 },
-    { src: "/images/ring.png", top: "88%", left: "68%", size: 120, rotate: -5, opacity: 0.25 },
     { src: "/images/wineglasses.png", top: "80%", left: "88%", size: 130, rotate: 5, opacity: 0.55 },
 
   ];
